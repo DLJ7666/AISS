a97e9aa451f5178048b571ee201b381f3cc17066
DLJ7666 <jarguezduran@gmail.com>
2024-05-07 12:13:03 +0200

    añadida uri e id a VimeoPicture

--- src/main/java/vimeominer/model/VimeoPicture.java
+++ src/main/java/vimeominer/model/VimeoPicture.java
@@ -6,8 +6,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class VimeoPicture {
 
+    @JsonProperty("uri")
+    private String uri;
+
     @JsonProperty("base_link")
     private String link;
+
+
+    public String getUri() { return uri; }
+
+    public void setUri(String uri) { this.uri = uri; }
 
     public VimeoPicture(String link) {
         this.link = link;
@@ -21,10 +29,12 @@ public class VimeoPicture {
         this.link = link;
     }
 
+    public String getId() { return this.uri.split("/")[4]; }
+
     @Override
     public String toString() {
-        return "VimeoPicture{" +
-                "link='" + link + '\'' +
+        return "VimeoPicture{id='" + getId() +
+                "', link='" + link + '\'' +
                 '}';
     }
 }
